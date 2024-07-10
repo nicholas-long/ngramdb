00396b847406d6cfee78a1001af7e60ffc7a961c
Nicholas Long <nicholas.eden.long@gmail.com>
2024-07-10 02:04:48 -0500

    create api web server

--- database/20240710061709-8857338e6983c4328948/README.md
+++ database/20240710061709-8857338e6983c4328948/README.md
@@ -1,6 +1,6 @@
 # new feature list
 
-- [ ] simple web server for API
+- [x] simple web server for API
 - [ ] TUI
 - [x] create with JSON parameter
 - [x] shortcut for tagging - include current ID from env `NGRAM_ID=20240710061709-8857338e6983c4328948`
--- database/20240710063214-e5c07cd186b492e8fb4b/server.js
+++ database/20240710063214-e5c07cd186b492e8fb4b/server.js
@@ -3,7 +3,13 @@ const app = express();
 const path = require('path');
 const port = 9000;
 
-app.use('/api', express.static(path.join(__dirname, '..')));
+var filedata = require('../20240710064155-0821472a1c9188ae741f/filepaths.json') // this is a link to the other data set
+
+for (const fp of filedata) {
+  const id_path = fp.replace('../', '/api/')
+  console.log(`serving ${fp} -> ${id_path}`)
+  app.use(id_path, express.static(path.join(__dirname, fp)));
+}
 
 app.listen(port, () => {
   console.log(`Server is listening on port ${port}`);
