162308a76fc7052fdc6aa9ab551843f2a2c837a8
Nicholas Long <nicholas.eden.long@gmail.com>
2024-07-10 03:11:01 -0500

    notify events fswatch

--- database/20240710063214-e5c07cd186b492e8fb4b/server.js
+++ database/20240710063214-e5c07cd186b492e8fb4b/server.js
@@ -2,8 +2,18 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const port = 9000;
+var child_process = require('child_process')
 
-var filedata = require('../20240710064155-0821472a1c9188ae741f/filepaths.json') // this is a link to the other data set
+const filepathsdatafile = '../20240710064155-0821472a1c9188ae741f/filepaths.json'
+var filedata = require(filepathsdatafile) // this is a link to the other data set
+
+// watch file for changes
+// TODO: this will work on mac and BSD, use inotifywait on linux
+var p = child_process.spawn('fswatch', [filepathsdatafile])
+p.stdout.on('data', ( data ) => {
+  console.log(`detected file changes, closing: ${data}`)
+  app.close();
+});
 
 for (const fp of filedata) {
   const id_path = fp.replace('../', '/api/')
